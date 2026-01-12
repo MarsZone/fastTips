f5b2b210569af6be32960d5b0d9828425bc5c301
Marszhang <zm03322161@qq.com>
2026-01-12 11:21:45 +0800

    update

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
+const url = require('url');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      // preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
       contextIsolation: false
     }
   });
 
-  // 加载应用
-  win.loadURL('http://localhost:5173');
-
-  // 开发环境下打开调试工具
+  // 根据环境加载不同的内容
   if (process.env.NODE_ENV === 'development') {
+    // 开发环境加载开发服务器
+    win.loadURL('http://localhost:5173');
+    // 打开调试工具
     win.webContents.openDevTools();
+  } else {
+    // 生产环境加载本地构建文件
+    win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, 'dist', 'index.html'),
+        protocol: 'file:',
+        slashes: true
+      })
+    );
   }
 }
 
